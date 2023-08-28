c34bc3d2681b95d7593ae4e837daab21730f1179
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-08-28 17:42:58 +1000

    Basic meme generator function is done including get random image from api, adding text over it

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -2,15 +2,20 @@ import React from "react";
 
 export default function Content(props){
     return (
-    <div>
-    <div className='memeForm'>
-        <input placeholder='Please enter the first line...'></input>
-        <input placeholder='Please enter the second line...'></input>
-        <button>Generate New Meme</button>
-      
+    <div className="Content">
+        <div className='memeForm'>
+            <input type="text" onChange={props.handleChange} name="firstText" value={props.firstText} placeholder='Please enter the first line...'></input>
+            <input type="text" onChange={props.handleChange} name="secondText" value={props.secondText} placeholder='Please enter the second line...'></input>
+            <button onClick={props.getImage}>Generate New Meme</button>
+        
 
-    </div>
-          {props.imageURL && <img src={props.imageURL}/>}
+        </div>
+        <div className="imageSection">
+        <h3 className="FirstText" >{props.firstText}</h3>
+        <h3 className="SecondText">{props.secondText}</h3>
+        {props.imageURL && <img className="Content-Image" src={props.imageURL}/>}
+  
+        </div>
     </div>
     );
 }
