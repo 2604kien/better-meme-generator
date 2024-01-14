26a45b1230b225e48edb91e6766de83e64105783
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-01-15 02:18:29 +1100

    able to set the position for the draggable content when state update

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -4,12 +4,20 @@ import DragText from "./DragText";
 export default function Content(props){
     const [numberText, setNumberText]=React.useState([1,2]);
     const [curr, setCurr]=React.useState(0);
+    const [textSize, setTextSize]=React.useState(2);
+    const[positionArray, setPositionArray]=React.useState([{x:0,y:0},{x:0,y:0}])
     const [formData, setFormData]=React.useState({
         text1: "",
         text2: ""
     })
+    const handlePositionDrag=(position, id)=>{
+        setPositionArray((prev)=>{
+            prev[id]=position
+            return prev;
+        })
+    }
     const dragElement=Object.values(formData).map(el=>{
-        return <DragText key={el} text={el}/>
+        return <DragText key={el+Math.random()} text={el} textSize={textSize} position={positionArray[Object.values(formData).findIndex(a=>a===el)]} handlePositionDrag={handlePositionDrag} id={Object.values(formData).findIndex(a=>a===el)}/>
     })
     const handleChange=(e)=>{
         const {name, value}=e.target;
@@ -20,7 +28,6 @@ export default function Content(props){
             }
         })
     }
-    console.log(formData);
     let inputElement=numberText.map(el=><input key={el} type="text" onChange={handleChange} name={`text${el}`} placeholder="Please enter something..." value={formData[`text${el}`]}/>)
     const handleAdd=()=>{
        
@@ -34,8 +41,15 @@ export default function Content(props){
                 ...prev,
                 [`text${numberText.length}`]:""
             }
+        });
+        setPositionArray(prev=>{
+            return[...prev, {x:0, y:0}]
         })
         
+    }
+    const handleFontSize=(e)=>{
+        const {value}=e.target
+        if(value>=1 && value<10) setTextSize(value)
     }
     const handleMinus=()=>{
         if(numberText.length>2){
@@ -46,6 +60,10 @@ export default function Content(props){
             setCurr(prev=>prev-1);
             setFormData(prev=>{
                 delete prev[`text${numberText.length+1}`];
+                return prev;
+            })
+            setPositionArray(prev=>{
+                prev.pop()
                 return prev;
             })
         }
@@ -59,20 +77,38 @@ export default function Content(props){
     }}>
         
         <div className='memeForm'>
-        <div style={{display:"flex", flexDirection:"row", gap:"5px", justifyContent:"center", alignItems:"center", marginTop:"80px", gap:"20px", width:"100%", height:"3rem"}}>
-            <h3>Number of line:</h3>
-                <button onClick={handleMinus} style={{
-                    width:"30px",
-                    height:"30px"
-                }}>-</button>
+        <div style={{width:"100%", display:"flex", flexDirection:"column", justifyContent:"center", marginTop:"40px", alignItems:"center"}}>
+        <h4>Number of line (minimum is 2):</h4>
+        <div style={{display:"flex", flexDirection:"row", gap:"5px", justifyContent:"center", alignItems:"center", gap:"20px", width:"100%"}}>
+            
+                <div onClick={handleMinus} style={{
+                    backgroundColor:"transparent",
+                    cursor:"pointer",
+                    fontSize:"2rem"
+                    
+                }}><i className="fa-solid fa-minus"></i></div>
                 <h3>{numberText.length}</h3>
-                <button onClick={handleAdd} style={{
-                    width:"30px",
-                    height:"30px"
-                }}>+</button>
+                <div onClick={handleAdd} style={{
+                    backgroundColor:"transparent",
+                    cursor:"pointer",
+                    fontSize:"1.8rem"
+                    
+                }}><i className="fa-solid fa-add"></i></div>
 
             </div>
-                {inputElement}
+            </div>
+            <div style={{
+                display:"flex",
+                flexDirection:"row",
+                gap:"10px",
+                justifyContent:"center",
+                alignItems:"center"
+            }}>
+                <h4>Font Size (min value is 1 and max value is 9):</h4>
+                <input type="number" onChange={handleFontSize} value={textSize} style={{width:"fit-content", fontSize:"1.5rem", textAlign:"center"}}/>
+                <h4>rem</h4>
+            </div>
+            {inputElement}
             <button onClick={props.getImage}>Generate New Meme</button>
 
         </div>
