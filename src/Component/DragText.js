cf71b2c06ddf8bb912e6d94bbee4374ea76afa0f
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-01-15 02:18:29 +1100

    able to set the position for the draggable content when state update

--- src/Component/DragText.js
+++ src/Component/DragText.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Draggable from "react-draggable";
-export default function DragText(props){
-    return(
+
+export default function DragText(props) {
+    const [position, setPosition] = useState({ x: props.position.x, y: props.position.y });
+
+    useEffect(() => {
+        setPosition({ x: props.position.x, y: props.position.y });
+    }, [props.position]);
+
+    const handleDrag = (e, data) => {
+        const newPosition = { x: data.x, y: data.y };
+        setPosition(newPosition);
+        props.handlePositionDrag(newPosition, props.id);
+    };
+
+    return (
         <Draggable
-        axis="both"
-        handle=".FirstText"
-        bounds="parent"
-        defaultPosition={{x: 0, y: 0}}
-        position={null}
-        grid={[1, 1]}
-        scale={1}>
-        <h3 className="FirstText" >{props.text}</h3>
-        </Draggable>  
-    )
+            axis="both"
+            handle=".FirstText"
+            bounds="parent"
+            defaultPosition={position}
+            grid={[1, 1]}
+            scale={1}
+            onDrag={handleDrag}
+        >
+            <h3 className="FirstText" style={{ fontSize: props.textSize + "rem" }}>
+                {props.text}{" "}
+            </h3>
+        </Draggable>
+    );
 }
